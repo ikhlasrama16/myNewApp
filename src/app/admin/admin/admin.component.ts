0cbf0a4cc822cec3dbc14746f75b3830ebba6663
1900016095 - Muhamad Ikhlas Ramadhan <muhamad1900016095@webmail.uad.ac.id>
2021-06-26 21:24:01 +0700

    progress week 5

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -11,5 +11,23 @@ export class AdminComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  menu=[
+    {
+      name:'Dashboard',
+      icon:'dashboard',
+      url:'/admin/dashboard'
+    },
+    {
+      group:'Menu Group',
+      children:[
+        {
+          name:'Image Gallery',
+          icon:'images',
+          url:'/admin/gallery'
+        }
+      ]
+    }
+  ];
+
 
 }
