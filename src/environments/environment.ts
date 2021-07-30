2e4483b5ed12954a7ec64f8ff5bc17aca6ace773
1900016095 - Muhamad Ikhlas Ramadhan <muhamad1900016095@webmail.uad.ac.id>
2021-07-30 20:52:21 +0700

    final

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
- 
   firebase : {
     apiKey: "AIzaSyDlXXgkTGPqvJl1Jjl10Og9HNwUp9o7YpA",
     authDomain: "tproject-845e1.firebaseapp.com",
@@ -12,7 +11,7 @@ export const environment = {
     storageBucket: "tproject-845e1.appspot.com",
     messagingSenderId: "820932151122",
     appId: "1:820932151122:web:d16b674f8b94bc675a1c8b"
-  }
+  },
 };
 
 /*
